a7d9d96d5ec3e5bf7d18c0c347e319bd8cf99bdb
sravya kalakuntla <kalakuntlasravya@gmail.com>
2021-03-14 18:34:42 +0530

    [sravya]ADD: calculate Daily Employee Wage feature implimented

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,16 +5,21 @@ namespace EmployeeWageComputation
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
             Console.WriteLine("Welcome To Employee Wage Computation");
             //constants
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            //variables
+            int empHrs = 0, empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             else
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine(" Employee Wage UC2 : " + empWage);
         }
     }
 }
