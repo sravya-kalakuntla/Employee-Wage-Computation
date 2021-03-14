8ade9aab0238baea2b03727f46c011dd64c63d03
sravya kalakuntla <kalakuntlasravya@gmail.com>
2021-03-14 20:03:21 +0530

    [sravya]ADD: Compute Employee Wage And Save Total Wage  feature implimented

---
+++ EmployeeWageComputation/Class4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    class EmployeeWageComputation
+    {
+
+        static void Main(string[] args)
+        {
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
+        }
+    }
+}
