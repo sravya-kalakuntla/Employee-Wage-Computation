c438d319789b9bb4765ba2a21ca6b8da85e97bc3
sravya kalakuntla <kalakuntlasravya@gmail.com>
2021-03-14 19:54:42 +0530

    [sravya]ADD: Compute Employee Wage for Multiple Companies feature implimented

---
+++ EmployeeWageComputation/Class3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    class EmployeeWageComputation
+    {
+        //constants
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            //variables
+            int empHrs = 0, totEmpHrs = 0, totWorkingDays = 0;
+            //computation
+            while (totEmpHrs <= maxHoursPerMonth &&
+                 totWorkingDays < numOfWorkingDays)
+            {
+                totWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                totEmpHrs += empHrs;
+                Console.WriteLine("Day# :" + totWorkingDays + "    Employee hours: " + empHrs);
+
+            }
+            int totEmpWage = totEmpHrs * empRatePerHour;
+            Console.WriteLine(" Total Employee Wage for company  : " + company + "is :" + totEmpWage);
+            return totEmpWage;
+        }
+        static void Main(string[] args)
+        {
+            computeEmpWage("DMart", 20, 2, 10);
+            computeEmpWage("Reliance", 10, 4, 20);
+        }
+    }
+}
