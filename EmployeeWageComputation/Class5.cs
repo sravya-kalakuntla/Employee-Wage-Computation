eecc20bba10dbc7d3b389ace9f016d673bde0576
sravya kalakuntla <kalakuntlasravya@gmail.com>
2021-03-14 20:10:47 +0530

    [sravya]ADD:Employee Wage For Multiple Company Using Interface feature implimented

---
+++ EmployeeWageComputation/Class5.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    class EmployeeWageComputation
+    {
+
+        static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.ComputeEmpWage();
+    }
+    }
+}
