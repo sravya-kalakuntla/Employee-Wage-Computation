1690e0160c88edb9c7ca6cca93c22f7379108eeb
sravya kalakuntla <kalakuntlasravya@gmail.com>
2021-03-14 18:58:01 +0530

    [sravya]ADD: Add PUse Case Statement feature implimented

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,28 +4,30 @@ namespace EmployeeWageComputation
 {
     class Program
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage Computation");
-            //constants
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
             //variables
             int empHrs = 0, empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
-                empHrs = 4;
-
-            else if (empCheck == IS_FULL_TIME)
-                empHrs = 8;
-
-            else
-                empHrs = 0;
-
+            switch ((int)empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine(" Employee Wage UC3 : " + empWage);
+            Console.WriteLine(" Employee Wage UC4 : " + empWage);
         }
     }
 }
