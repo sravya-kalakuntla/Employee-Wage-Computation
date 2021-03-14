96a9bdd93ab175417df563801d3927b9d9efa59c
sravya kalakuntla <kalakuntlasravya@gmail.com>
2021-03-14 18:24:03 +0530

    [sravya]ADD: Employee Attendencs feature implimented

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,9 +5,16 @@ namespace EmployeeWageComputation
     class Program
     {
         static void Main(string[] args)
-        {
-
+        { 
             Console.WriteLine("Welcome To Employee Wage Computation");
+            //constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
         }
     }
 }
